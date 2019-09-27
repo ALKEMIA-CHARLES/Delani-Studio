2682e0ad9e96e90b1ab7683f4051a87c26a669e7
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2019-09-27 16:32:55 +0300

    added.value

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,19 @@
-$(document).ready(function() {
-  $("#despic").click(function() {
-    $("#pentag").toggle();
-  });
-  $("#devpic").click(function() {
-    $("#codetag").toggle();
-  });
+$(document).ready(function(event) {
+    event.preventDefault;
+    $("#despic").click(function() {
+        $("#pentag").toggle();
+    });
+    $("#devpic").click(function() {
+        $("#codetag").toggle();
+    });
 
-  $("#prodpic").click(function() {
-    $("#listtag").toggle();
-  });
+    $("#prodpic").click(function() {
+        $("#listtag").toggle();
+    });
+
+    $("#contactid").submit(function() {
+        var user = $('#name').val();
+        alert(user + " your insights have been well recieved, we will keep in touch");
+    });
 });
 
-$("#contactid").submit(function() {
-  event.preventDefault;
-  var user = document.getElementById("name").value;
-  alert(user + "Your insights have been well recieved, we will keep in touch");
-});
