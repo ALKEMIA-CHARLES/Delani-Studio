2f15c2e03fa7df60f661bea6ee6035907dc7a4d0
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2019-09-27 09:44:00 +0300

    added functions

---
+++ js/scripts.js
@@ -0,0 +1,26 @@
+$(document).ready(function () {
+    $("#despic").click(function () {
+        $("#despic").toggle(function () {
+        });
+            $("#pentag").show(function () {
+          
+        });
+    });
+    $("#devpic").click(function () {
+        $("#devpic").toggle(function () {
+            $(".codetag").show(function () {
+
+            });
+        });
+    });
+
+    $("#prodpic").click(function () {
+        $("#prodpic").toggle(function () {
+            
+            (".listtag").show(function () {
+
+            });
+        });
+    });
+
+});
